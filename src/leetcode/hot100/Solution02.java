dcb1fe6fa69c8dc74ea156ed039a6f5da9e6ef75
clarklyY <a2868451714@gmail.com>
2022-02-09 22:57:09 +0800

    02,03

---
+++ src/leetcode/hot100/Solution02.java
@@ -0,0 +1,28 @@
+package leetcode.hot100;
+
+import leetcode.ListNode;
+
+public class Solution02 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry =0;
+        ListNode head=null, tail=null;
+        while(l1!=null||l2!=null){
+            int v1 = l1 == null ? 0 : l1.val;
+            int v2 = l2 == null ? 0 : l2.val;
+            int sum = v1+v2+carry;
+            if(head==null){
+                head = tail = new ListNode(sum %10);
+            }else{
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum/10;
+            if(l1!=null) l1=l1.next;
+            if(l2!=null) l2=l2.next;
+        }
+        if(carry>0){
+            tail.next = new ListNode(1);
+        }
+        return head;
+    }
+}
---
+++ src/leetcode/hot100/Solution03.java
@@ -0,0 +1,22 @@
+package leetcode.hot100;
+
+import java.util.HashSet;
+
+public class Solution03 {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left=0, right=0, max=0;
+        while(right<s.length()){
+            char rightChar = s.charAt(right);
+            right++;
+            while(set.contains(rightChar)){
+                char leftChar = s.charAt(left);
+                left++;
+                set.remove(leftChar);
+            }
+            set.add(rightChar);
+            if(set.size()>max) max=set.size();
+        }
+        return max;
+    }
+}
